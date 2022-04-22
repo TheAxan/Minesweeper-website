0c6ccb898e799adee5d1d8bec88d45b909b559ef
TheAxan <88592270+TheAxan@users.noreply.github.com>
2022-04-21 21:35:50 -0400

    switched to using a table
    basic reorganisation

--- myScript.js
+++ myScript.js
@@ -11,13 +11,14 @@ for (let y = 0; y <= height; y++) {
     let row = [];
     for (let x = 0; x <= width; x++) {
         row.push(
-            `<button type='button' class='btn btn-primary tile' ${sweep_action}='sweep(${x}, ${y})' ${flag_action}='flag(${x}, ${y})'>&nbsp</button>`
-        );
+            `<td><button type='button' class='btn btn-primary tile' ${sweep_action}='sweep(${x}, ${y})' ${flag_action}='flag(${x}, ${y})'></button></td>`
+        );  
     };
-    row.push('<br>');
-    output.push(row.join(''));
+    output.push(`<tr>${row.join('')}</tr>`);
 };
 output = output.join('');
+
+document.getElementById('minefield').innerHTML = output;
 
 
 function getRandomInt(min, max) {
@@ -27,7 +28,7 @@ function getRandomInt(min, max) {
 };
 
 
-let mineField = new Array(height)
+let minefield = new Array(height)
     .fill(null)
     .map(
         () => Array(width).fill(0)
@@ -38,18 +39,15 @@ let fillRatio = 0.2;
 for (let placed = 0; placed <= height * width * fillRatio;) {
     let x = getRandomInt(0, width);
     let y = getRandomInt(0, height);
-    if (mineField[y][x] < 9) {
-        mineField[y][x] = 9;
+    if (minefield[y][x] < 9) {
+        minefield[y][x] = 9;
         for (let [j, i] of [[-1, -1], [-1, 0], [-1, 1], 
                                                 [0, -1], [0, 1], 
                                                 [1, -1], [1, 0], [1, 1]]) {
             if (-1 < y+j && y+j < height && -1 < x+i && x+i < width) {
-                mineField[y+j][x+i] += 1;
+                minefield[y+j][x+i] += 1;
             };
         };
         placed++;
     };
 };
-
-
-document.getElementById('array').innerHTML = output;
