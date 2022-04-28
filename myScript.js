ee9ab09ffdcf4f61781f148d94b7d399aba4e1d0
TheAxan <88592270+TheAxan@users.noreply.github.com>
2022-04-28 15:59:54 -0400

    Turned existing script into function

--- myScript.js
+++ myScript.js
@@ -1,25 +1,17 @@
-let height = 10, width =10;
-
-
-let output = [];
-
-let sweep_action = 'onclick';
-
-let flag_action = 'oncontextmenu';
-
-for (let y = 0; y <= height; y++) {
-    let row = [];
-    for (let x = 0; x <= width; x++) {
-        row.push(
-            `<td><button type='button' class='btn btn-primary tile' ${sweep_action}='sweep(${x}, ${y})' ${flag_action}='flag(${x}, ${y})'></button></td>`
-        );  
+function generateMinefieldTable(height = 10, width = 10, sweep_action = 'onclick', flag_action = 'oncontextmenu') {
+    let output = [];
+    for (let y = 0; y <= height; y++) {
+        let row = [];
+        for (let x = 0; x <= width; x++) {
+            row.push(
+                `<td><button type='button' class='btn btn-primary tile' ${sweep_action}='sweep(${x}, ${y})' ${flag_action}='flag(${x}, ${y})'></button></td>`
+            );  
+        };
+        output.push(`<tr>${row.join('')}</tr>`);
     };
-    output.push(`<tr>${row.join('')}</tr>`);
+    output = output.join('');
+    document.getElementById('minefield').innerHTML = output;
 };
-output = output.join('');
-
-document.getElementById('minefield').innerHTML = output;
-
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -27,27 +19,28 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 };
 
+function generateMinefieldArray(height = 10, width = 10) {
+    let minefield = new Array(height)
+        .fill(null)
+        .map(
+            () => Array(width).fill(0)
+        );
 
-let minefield = new Array(height)
-    .fill(null)
-    .map(
-        () => Array(width).fill(0)
-    );
+    let fillRatio = 0.2;
 
-let fillRatio = 0.2;
-
-for (let placed = 0; placed <= height * width * fillRatio;) {
-    let x = getRandomInt(0, width);
-    let y = getRandomInt(0, height);
-    if (minefield[y][x] < 9) {
-        minefield[y][x] = 9;
-        for (let [j, i] of [[-1, -1], [-1, 0], [-1, 1], 
-                                                [0, -1], [0, 1], 
-                                                [1, -1], [1, 0], [1, 1]]) {
-            if (-1 < y+j && y+j < height && -1 < x+i && x+i < width) {
-                minefield[y+j][x+i] += 1;
+    for (let placed = 0; placed <= height * width * fillRatio;) {
+        let x = getRandomInt(0, width);
+        let y = getRandomInt(0, height);
+        if (minefield[y][x] < 9) {
+            minefield[y][x] = 9;
+            for (let [j, i] of [[-1, -1], [-1, 0], [-1, 1], 
+                                [0, -1], [0, 1], 
+                                [1, -1], [1, 0], [1, 1]]) {
+                if (-1 < y+j && y+j < height && -1 < x+i && x+i < width) {
+                    minefield[y+j][x+i] += 1;
+                };
             };
+            placed++;
         };
-        placed++;
     };
 };
