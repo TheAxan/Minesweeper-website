08c1b7047831c4f2df3a1479d3b93e04aadef265
TheAxan <88592270+TheAxan@users.noreply.github.com>
2022-05-25 16:55:35 -0400

    reformating

--- myScript.js
+++ myScript.js
@@ -12,7 +12,14 @@ function generateMinefieldTable(targetId) {
         for (let x = 0; x < gridWidth; x++) {
             row.push(
                 `<td id='cell-${x}-${y}'  height='31px' width='31px'>
-					<button id='button-${x}-${y}' type='button' class='btn btn-primary tile' ${sweepAction}='sweep(${x}, ${y}); event.stopPropagation(); return false;' ${flagAction}='flag(${x}, ${y}); return false;'></button>
+					<button id='button-${x}-${y}' type='button'
+                            class='btn btn-primary tile'
+                            ${sweepAction}='sweep(${x}, ${y});
+                                            event.stopPropagation();
+                                            return false;'
+                            ${flagAction}='flag(${x}, ${y});
+                                           return false;'
+                    ></button>
 				</td>`
             );  
         };
@@ -30,16 +37,11 @@ function getRandomInt(min, max) {
 };
 
 
-function forNeighbors(x, y, func) {
-    for (let [i, j] of neighborsArray(x, y)) {
-        func(i, j);
-    };
-};
-
-
 function neighborsArray(x, y) {
     let output = new Array;
-    for (let [i, j] of [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]) {
+    for (let [i, j] of [
+        [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
+    ]) {
         if (-1 < y+j && y+j < gridHeight && -1 < x+i && x+i < gridWidth) {
            output.push([x+i, y+j]);
         };
@@ -68,7 +70,9 @@ function generateMinefieldArray() {
         let y = getRandomInt(0, gridHeight);
         if (minefield[y][x] < 9) {
             minefield[y][x] = 9;
-            forNeighbors(x, y, raiseMinecount);
+            for (let [i, j] of neighborsArray(x, y)) {
+                raiseMinecount(i, j);
+            };
             placed++;
         };
     };
@@ -93,23 +97,26 @@ function checkForWin() {
 
 
 function generatePlayAgainButton(){
-    document.getElementById('minefield').insertAdjacentHTML(
-        'beforebegin',
-        '<br><button class="btn btn-primary m-2" href="#" onclick="location.reload(true); return false;">Play again</button>'
-        );
+    document.getElementById('minefield').insertAdjacentHTML('beforebegin',
+        `<br>
+        <button class="btn btn-primary m-2" href="#" 
+                onclick="location.reload(true); return false;">
+          Play again
+        </button>`
+    );
 };
 
-var gameOver = false;
-var gameStarted = false;
+
 var explored = new Set();
-function sweep(x, y, recursive=false) {
-    if (gameOver) {
-        return;
-    } else if (!gameStarted) {
-        regenerateMinefield(x, y);
-        gameStarted = true;
-        sweep(x, y);
-    } else if (!explored.has(String([x, y]))) {
+function sweep(x, y) {
+    regenerateMinefield(x, y);
+    sweep = gameStartedSweep;
+    sweep(x, y);
+};
+
+
+function gameStartedSweep(x, y, recursive=false) {
+    if (!explored.has(String([x, y]))) {
         let cell = document.getElementById(`cell-${x}-${y}`);
         let cellValue = minefield[y][x];
         if (cellValue === 0) {
@@ -120,17 +127,22 @@ function sweep(x, y, recursive=false) {
             };
         } else if (cellValue === 9) {
             cell.innerHTML = mineSVG('#FFFFFF');
-            gameOver = true;
+            sweep = flag = gameOverAction;
             generatePlayAgainButton();
         } else {
             cell.innerHTML = cellValue;
-            cell.setAttribute(sweepAction, `sweep(${x}, ${y}); event.stopPropagation(); return false;`)
+            cell.setAttribute(
+                sweepAction, 
+                `sweep(${x}, ${y});
+                event.stopPropagation();
+                return false;`
+            )
             explored.add(String([x, y]));
         };
         checkForWin();
-    } else if (!recursive) {  // explored and manual sweep
+    } else if (!recursive) {  // manual sweep
         if (document.getElementById(`button-${x}-${y}`) != null) { // flagged
-            flag(x, y);
+            flag(x, y);  // unflagging
         } else { // chaining
             for (let [i, j] of neighborsArray(x, y)) {
                 sweep(i, j, true);
@@ -140,31 +152,41 @@ function sweep(x, y, recursive=false) {
 };
 
 
-function flag(x, y) {
-    if (gameOver) {
-        return;
-    } else {
-        let button = document.getElementById(`button-${x}-${y}`);
-        if (button.innerHTML == '') {
-            button.innerHTML = flagSVG('#FFFFFF');
-            mineCounter--;
-            generateMineCounter('mineCounter', '#FFFFFF');
-            explored.add(String([x, y]));
-        } else {
-            button.innerHTML = '';
-            mineCounter++;
-            generateMineCounter('mineCounter', '#FFFFFF');
-            explored.delete(String([x, y]));
-        };
-        checkForWin();
-    };
+function gameOverAction() {
+    return;
 };
+
+
+function flag(x, y) {
+    let button = document.getElementById(`button-${x}-${y}`);
+    if (button.innerHTML == '') {
+        button.innerHTML = flagSVG('#FFFFFF');
+        mineCounter--;
+        generateMineCounter();
+        explored.add(String([x, y]));
+    } else {
+        button.innerHTML = '';
+        mineCounter++;
+        generateMineCounter();
+        explored.delete(String([x, y]));
+    };
+    checkForWin();
+};
+
 
 
 function flagSVG(color = '#000000'){
 	return `<svg viewBox="0 0 52 91" height="17" class="d-inline-block">
 		<defs>
-			<style>.flagStyle{stroke:${color};fill:${color};stroke-linecap:round;stroke-linejoin:round;stroke-width:12px;}</style>
+			<style>
+                .flagStyle{
+                    stroke:${color};
+                    fill:${color};
+                    stroke-linecap:round;
+                    stroke-linejoin:round;
+                    stroke-width:12px;
+                }
+            </style>
 		</defs>
 		<line class="flagStyle" x1="46" y1="85" x2="46" y2="46"/>
 		<polygon class="flagStyle" points="6 26 46 46 46 6 6 26"/>
@@ -175,7 +197,14 @@ function flagSVG(color = '#000000'){
 function gearSVG(color = '#000000'){
 	return `<svg viewBox="0 0 80.97 80.97" width="60">
 		<defs>
-			<style>.gearStyle{fill:${color};stroke:${color};stroke-linejoin:round;stroke-width:4px;}</style>
+			<style>
+                .gearStyle{
+                    fill:${color};
+                    stroke:${color};
+                    stroke-linejoin:round;
+                    stroke-width:4px;
+                }
+            </style>
 		</defs>
 		<path class="gearStyle" d="M40.49,30.49c.06,0,.13,0,.19,0L28.87,2l-7.39,3.06,11.8,28.5c1.82-1.89,4.37-3.07,7.2-3.07Z"/>
 		<path class="gearStyle" d="M47.69,47.42c-1.82,1.89-4.37,3.07-7.2,3.07-.06,0-.13,0-.19,0l11.8,28.5,7.39-3.06-11.8-28.5Z"/>
@@ -195,35 +224,48 @@ function generateGear(targetId, color ='#000000'){
 };
 
 
-function mineSVG(color = '#000000', height = '25', specifierInfo = ''){
+function mineSVG(color = '#000000', height = '25', specifier = ''){
 	return `<svg viewBox="0 0 90 90" height=${height} class="d-inline-block">
 		<defs>
 			<style>
-                .mineCircle${specifierInfo}Style, .mineLine${specifierInfo}Style {fill:${color} !important;stroke:${color} !important;stroke-miterlimit:10;}
-                .mineLine${specifierInfo}Style {stroke-linecap:round;stroke-width:10px;}
+                .mineCircle${specifier}Style, .mineLine${specifier}Style {
+                    fill:${color} !important;
+                    stroke:${color} !important;
+                    stroke-miterlimit:10;
+                }
+                .mineLine${specifier}Style {
+                    stroke-linecap:round;
+                    stroke-width:10px;
+                }
             </style>
 		</defs>
-		<circle class="mineCircle${specifierInfo}Style" cx="45" cy="45" r="25"/>
-		<line class="mineLine${specifierInfo}Style" x1="85" y1="45" x2="5" y2="45"/>
-		<line class="mineLine${specifierInfo}Style" x1="73.28" y1="16.72" x2="16.72" y2="73.28"/>
-		<line class="mineLine${specifierInfo}Style" x1="45" y1="5" x2="45" y2="85"/>
-		<line class="mineLine${specifierInfo}Style" x1="16.72" y1="16.72" x2="73.28" y2="73.28"/>
+		<circle class="mineCircle${specifier}Style" cx="45" cy="45" r="25"/>
+		<line class="mineLine${specifier}Style" x1="85" y1="45" x2="5" y2="45"/>
+		<line class="mineLine${specifier}Style" x1="73.28" y1="16.72" x2="16.72" y2="73.28"/>
+		<line class="mineLine${specifier}Style" x1="45" y1="5" x2="45" y2="85"/>
+		<line class="mineLine${specifier}Style" x1="16.72" y1="16.72" x2="73.28" y2="73.28"/>
 	</svg>`;
 };
 
 
 function generateMineHeader(targetId, color = '#000000'){
-	document.getElementById(targetId).insertAdjacentHTML('afterbegin', String(mineSVG(color, 100, 'Icon')));
+	document.getElementById(targetId).insertAdjacentHTML(
+        'afterbegin',
+        String(mineSVG(color, 100, 'Icon'))
+    );
 };
 
 
-function generateMineCounter(targetId, color){
-    document.getElementById(targetId).innerHTML = mineCounter + ' ' + mineSVG(color);
+function generateMineCounter(targetId = 'mineCounter', color = '#FFFFFF'){
+    document.getElementById(targetId)
+        .innerHTML = mineCounter + ' ' + mineSVG(color);
 };
 
 
 // enable tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipTriggerList = [].slice.call(document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 });
@@ -233,4 +275,4 @@ generateMinefieldTable('minefield');
 generateMinefieldArray();
 generateGear('gear', '#FFFFFF');
 generateMineHeader('mineHeader','#FFFFFF');
-generateMineCounter('mineCounter', '#FFFFFF');
+generateMineCounter();
